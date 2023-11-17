03dfc1f8e0be3f16a62a8b95b071d41a8d8f16fe
YoselC <Yosel.cepedaz@gmail.com>
2023-11-17 08:39:32 -0600

    done
    done

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    [SerializeField] private AudioSource deathSoundEffect;
+    private Rigidbody2D rb;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+    private void OnCollisionEnter2D(Collision2D coliision)
+    {
+        if (coliision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+    private void Die()
+    {
+        deathSoundEffect.Play();
+        rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("death");
+    }
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
